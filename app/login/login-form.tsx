b795f7b8620c339adc225cde6947c66563e521a9
VincentmutakoowA <vincentmutakoowa@gmail.com>
2026-02-20 10:46:17 +0300

    Update

--- app/login/login-form.tsx
+++ app/login/login-form.tsx
@@ -27,7 +27,7 @@ export function LoginForm({ className, loginAction, signupAction,}: LoginFormPro
           <form className="p-6 md:p-8">
             <FieldGroup>
               <div className="flex flex-col items-center gap-2 text-center">
-                <h1 className="text-2xl font-bold">Welcome back</h1>
+                <h1 className="text-2xl font-bold">Welcome </h1>
               </div>
 
               <Field>
